a284f2cbd2036048e36eef35bfa7d485b5c6b8ec
Rene Larios <112102394+Shue-Larios@users.noreply.github.com>
2023-09-27 02:27:49 -0600

    Primer commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Home = () => {
+    return (
+        <>
+            <div className='w-3/4 text-center px-5 mx-auto pb-3 m-5'>
+                <div className='inline-block bg-red-500'>
+                    <h1 className='text-white'>Nota: Actualmente estamos trabajando diligentemente en la expansión y mejora continua de nuestro portafolio</h1>
+                </div>
+            </div>
+
+
+        </>
+    )
+}
---
+++ src/components/Trabajos.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export const Trabajos = ({ title, description, url, img }) => {
+
+
+
+    return (
+        <>
+      
+            <div className="p-7 max-w-md rounded bg-white overflow-hidden shadow-lg">
+                <img className="w-full" src={img} alt={title} />
+                <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">{title}</div>
+                    <p className="text-gray-700 text-base">{description}</p>
+                </div>
+                <div className="px-6 pt-4 pb-2">
+                    <a
+                        href={url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+                    >
+                        Ver Proyecto
+                    </a>
+                </div>
+            </div>
+
+
+
+        </>
+
+    )
+}
