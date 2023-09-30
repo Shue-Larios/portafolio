ab51c6a86ad87327308c3787257dcdeb0dcee307
Rene Larios <112102394+Shue-Larios@users.noreply.github.com>
2023-09-30 00:21:44 -0600

    navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,103 @@
-import React from 'react'
+import {  useState } from 'react'
 
 export const Navbar = () => {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+
+  
+
     return (
         <>
-            <div className='text-white flex py-2 pr-10 justify-end'>
+            {/* <div className='text-white flex py-2 pr-10 justify-end'>
                 <a href="/" className='px-4 py-2 hover:bg-gray-700 hover:rounded-full'>Home</a>
                 <a href="acerca" className='px-4 py-2 hover:bg-gray-700 hover:rounded-full'>Acerca de</a>
                 <a href="habilidades" className='px-4 py-2 hover:bg-gray-700 hover:rounded-full'>Habilidad</a>
                 <a href="trabajos" className='px-4 py-2 hover:bg-gray-700 hover:rounded-full'>Trabajos</a>
                 <a href="contacto" className='px-4 py-2 hover:bg-gray-700 hover:rounded-full'>Contacto</a>
-            </div>
+            </div>   */}
 
+
+<div className='relative '>
+      <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+        <div className='flex items-center justify-between h-16'>
+
+            {/* lado izquierdo */}
+          <div className='flex items-center'>
+            <a href='/' className='text-white text-xl font-bold'>
+            <img
+              className='w-24 h-auto'
+              src="https://firebasestorage.googleapis.com/v0/b/restaurant-63fb0.appspot.com/o/imagenes%2FLogo%20Rene%20LArios.png?alt=media&token=40880050-efd0-47c0-916c-7b170ef92f1d&_gl=1*rud38e*_ga*NjU3MzEyOTI1LjE2OTU3OTkyNzM.*_ga_CW55HF8NVT*MTY5NjA1NDA1My41LjEuMTY5NjA1NDYwMy41Mi4wLjA." alt="Logo Personal"
+              title='Logo Rene Larios'
+              />
+            </a>
+          </div>
+
+          {/* lado derecho */}
+          <div className='hidden md:flex space-x-4'>
+            <a href='/' className='text-white hover:bg-gray-700 px-4 py-2 rounded-full'>
+              Home
+            </a>
+            <a href='acerca' className='text-white hover:bg-gray-700 px-4 py-2 rounded-full'>
+              Acerca de
+            </a>
+            <a href='habilidades' className='text-white hover:bg-gray-700 px-4 py-2 rounded-full'>
+              Habilidad
+            </a>
+            <a href='trabajos' className='text-white hover:bg-gray-700 px-4 py-2 rounded-full'>
+              Trabajos
+            </a>
+            <a href='contacto' className='text-white hover:bg-gray-700 px-4 py-2 rounded-full'>
+              Contacto
+            </a>
+          </div>
+          <div className='md:hidden flex items-center'>
+            <button onClick={toggleMenu} className='text-gray-300 hover:text-white focus:outline-none'>
+              <svg
+                className='h-6 w-6'
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='M4 6h16M4 12h16m-7 6h7'
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* Floating Mobile Menu */}
+      {isMenuOpen && (
+        <div className='md:hidden absolute top-16 right-4 bg-gray-900 py-2 px-4 rounded-lg'>
+          <a href='/' className='block text-white hover:bg-gray-700 px-4 py-2 rounded-full'>
+            Home
+          </a>
+          <a href='acerca' className='block text-white hover:bg-gray-700 px-4 py-2 rounded-full'>
+            Acerca de
+          </a>
+          <a href='habilidades' className='block text-white hover:bg-gray-700 px-4 py-2 rounded-full'>
+            Habilidad
+          </a>
+          <a href='trabajos' className='block text-white hover:bg-gray-700 px-4 py-2 rounded-full'>
+            Trabajos
+          </a>
+          <a href='contacto' className='block text-white hover:bg-gray-700 px-4 py-2 rounded-full'>
+            Contacto
+          </a>
+        </div>
+      )}
+    </div>
+          
 
         </>
     )
